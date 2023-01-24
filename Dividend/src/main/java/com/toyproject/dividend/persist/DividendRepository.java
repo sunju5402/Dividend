ec23c24d3fb218108c1b7385dcbfaffda3fc8f28
sunju5402 <ssonju5402@gmail.com>
2023-01-25 06:45:33 +0900

    feat: 스케줄러 구현 및 쓰레드 관리

--- Dividend/src/main/java/com/toyproject/dividend/persist/DividendRepository.java
+++ Dividend/src/main/java/com/toyproject/dividend/persist/DividendRepository.java
@@ -1,6 +1,7 @@
 package com.toyproject.dividend.persist;
 
 import com.toyproject.dividend.persist.entity.DividendEntity;
+import java.time.LocalDateTime;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -8,4 +9,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface DividendRepository extends JpaRepository<DividendEntity, Long> {
 	List<DividendEntity> findAllByCompanyId(Long companyId);
+	boolean existsByCompanyIdAndDate(Long companyId, LocalDateTime dateTime);
 }
---
+++ Dividend/src/main/java/com/toyproject/dividend/scheduler/ScraperScheduler.java
@@ -0,0 +1,59 @@
+package com.toyproject.dividend.scheduler;
+
+import com.toyproject.dividend.model.Company;
+import com.toyproject.dividend.model.ScrapedResult;
+import com.toyproject.dividend.persist.CompanyRepository;
+import com.toyproject.dividend.persist.DividendRepository;
+import com.toyproject.dividend.persist.entity.CompanyEntity;
+import com.toyproject.dividend.persist.entity.DividendEntity;
+import com.toyproject.dividend.scraper.YahooFinanceScraper;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@AllArgsConstructor
+public class ScraperScheduler {
+
+	private final CompanyRepository companyRepository;
+	private final DividendRepository dividendRepository;
+	private final YahooFinanceScraper yahooFinanceScraper;
+
+	@Scheduled(cron = "${scheduler.scrap.yahoo}")
+	public void yahooFinanceScheduling() {
+		// 저장된 회사 목록 조회
+		List<CompanyEntity> companies = companyRepository.findAll();
+
+		// 회사마다 배당금 정보를 새로 스크래핑
+		for (CompanyEntity company : companies) {
+			log.info("scraping scheduler is started -> " + company.getName());
+			ScrapedResult scrapedResult = yahooFinanceScraper.scrap(Company.builder()
+																		.name(company.getName())
+																		.ticker(company.getTicker())
+																		.build());
+
+			// 스크래핑한 배당금 정보 중 데이터베이스에 없는 값은 저장
+			scrapedResult.getDividends().stream()
+				// model -> entity mapping
+				.map(e -> new DividendEntity(company.getId(), e))
+				.forEach(e -> {
+					boolean exists = dividendRepository.existsByCompanyIdAndDate(e.getCompanyId(), e.getDate());
+					if (!exists) {
+						dividendRepository.save(e);
+					}
+				});
+
+			// 연속적으로 스크래핑 대상 사이트 서버에 요청을 날리지 않도록 일시정지
+			// for문 안에 써줘야 함.
+			try {
+				Thread.sleep(3000);
+			} catch (InterruptedException e) {
+				Thread.currentThread().interrupt();
+			}
+		}
+
+	}
+}
